aea7b7cbde1c013b6fbe94ace307832790bb070c
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2023-06-03 10:17:54 +0900

    📝Post about manupilating spatial data

--- docs/start-rstudio.ps1
+++
@@ -1,8 +0,0 @@
-# パスワードの入力
-$password = Read-Host "RStudioのパスワードを入力してください"
-# 環境変数へ設定
-$env:PASSWORD = $password
-# RStudioコンテナを起動
-docker-compose up -d --build
-# RStudioを起動
-Start-Process http://localhost:8787
